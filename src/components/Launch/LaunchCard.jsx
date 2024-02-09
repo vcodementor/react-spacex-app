7c6419fd8a9eca1b75e2bec949c7acb98c68ec09
vcodementor <vcodementor@gmail.com>
2024-02-09 16:07:55 -0500

    launch card ui improved

--- src/components/Launch/LaunchCard.jsx
+++ src/components/Launch/LaunchCard.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Card, CardMedia,CardActions, CardContent, Button, Typography } from '@mui/material';
+import { Card, CardMedia,CardActions, CardActionArea,CardHeader, Avatar,CardContent, Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 const LaunchCard = ({ launch }) => {
   return (
     <Card>
-      {/* <CardMedia
-        component="img"
-        alt={launch?.mission_name}
-        image={launch?.links?.mission_patch_small}
-      /> */}
-      <CardContent>
-        <Typography variant="h6" gutterBottom>{launch?.mission_name}</Typography>
-        <Typography variant="body2" gutterBottom> 🚀 {`${launch?.rocket?.rocket_name}`}</Typography>
-        {/* <Typography variant="body2" gutterBottom>{`${launch?.launch_site?.site_name}`}</Typography> */}
-        <Typography variant="body2" gutterBottom>{`${moment(launch?.launch_date_utc).format('MMMM Do YYYY, h:mm:ss a')}`}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button component={Link} to={`/launch/${launch?.id}`}  size="small" color="primary">Details</Button>
-      </CardActions>
+      <CardActionArea href={`/launch/${launch?.id}`}>
+        <CardHeader
+          avatar={
+            <Avatar
+              aria-label="recipe"
+              src={launch?.links?.mission_patch_small || ""}
+            />
+          }
+          title={launch?.mission_name}
+          subheader={`${moment(launch?.launch_date_local).format('MMMM Do YYYY, h:mm:ss a')}`}
+        />
+        <CardContent>
+          <Typography variant="body1" gutterBottom>{` 🚀 ${launch?.rocket?.rocket_name}`}</Typography>
+          {launch?.launch_site?.site_name ? <Typography variant="subtitle1" gutterBottom>{`${launch?.launch_site?.site_name }`}</Typography> : null }
+        </CardContent>
+        <CardActions>
+          <Button component={Link} to={`/launch/${launch?.id}`}  size="small" color="primary">Details</Button>
+        </CardActions>
+      </CardActionArea>
     </Card>
   );
 };
